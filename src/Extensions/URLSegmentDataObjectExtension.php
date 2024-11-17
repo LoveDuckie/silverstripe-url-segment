77b5c62f34fd480951e1fcb62ee4e5c9962695e2
Luc Shelton <lucshelton@gmail.com>
2024-11-17 12:24:46 +0000

    Fixing up the composer.json manifest

---
+++ src/Extensions/URLSegmentDataObjectExtension.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace LoveDuckie\SilverStripe\URLSegment\Extensions;
+
+use Exception;
+use Portfolio\Models\EventImage;
+use Portfolio\Models\ProjectImage;
+use Psr\Log\LoggerInterface;
+use SilverStripe\Core\Injector\Injector;
+use SilverStripe\ORM\DataExtension;
+use SilverStripe\View\Parsers\URLSegmentFilter;
+
+class URLSegmentDataObjectExtension extends DataExtension
+{
+    private static $db = [
+        'URLSegment' => 'Varchar(255)'
+    ];
+
+    public function onBeforeWrite(): void
+    {
+        parent::onBeforeWrite();
+
+        if (!$this->owner->hasField('URLSegment')) {
+            return;
+        }
+
+        $pageTitle = $this->getPageTitle();
+
+        if (!$this->owner->URLSegment) {
+            $this->owner->URLSegment = $this->generateUrlSegment($this->owner->Title);
+        }
+
+        if (!$this->owner->isInDB() || $this->owner->isChanged('Title', 2)) {
+            $this->owner->URLSegment = $this->generateUrlSegment($pageTitle ? "$pageTitle-{$this->owner->Title}" : $this->owner->Title);
+            $this->makeUrlSegmentUnique();
+        } elseif ($this->owner->isChanged('URLSegment', 2)) {
+            $this->owner->URLSegment = $this->generateUrlSegment($this->owner->URLSegment);
+            $this->makeUrlSegmentUnique();
+        }
+    }
+
+    private function getPageTitle(): ?string
+    {
+        if ($this->owner instanceof ProjectImage) {
+            return $this->owner->Project->Title ?? null;
+        }
+
+        if ($this->owner instanceof EventImage) {
+            return $this->owner->Event->Title ?? null;
+        }
+
+        return null;
+    }
+
+    public function onAfterWrite(): void
+    {
+        if ($this->owner instanceof EventImage || $this->owner instanceof ProjectImage) {
+            $this->updateFileName();
+        }
+    }
+
+    private function updateFileName(): void
+    {
+        $currentFileName = $this->owner->getFilename();
+        if (!$currentFileName) {
+            return;
+        }
+
+        $fileParts = pathinfo($currentFileName);
+        $urlSegment = strtolower($this->owner->URLSegment);
+
+        if (!str_starts_with($urlSegment, $fileParts['filename'])) {
+            $newFileName = "{$fileParts['dirname']}/{$urlSegment}.{$fileParts['extension']}";
+            Injector::inst()->get(LoggerInterface::class)->info("Renaming file to $newFileName");
+            $this->owner->renameFile($newFileName);
+        }
+    }
+
+    private function isUrlSegmentInUse(string $urlSegment): bool
+    {
+        $items = $this->owner::get()->filter('URLSegment', $urlSegment);
+
+        if ($this->owner->isInDB()) {
+            $items = $items->exclude('ID', $this->owner->ID);
+        }
+
+        return $items->exists();
+    }
+
+    private function makeUrlSegmentUnique(): void
+    {
+        $count = 2;
+        $currentURLSegment = $this->owner->URLSegment;
+
+        while ($this->isUrlSegmentInUse($currentURLSegment)) {
+            $currentURLSegment = preg_replace('/-\d+$/', '', $currentURLSegment) . "-$count";
+            $count++;
+        }
+
+        $this->owner->URLSegment = $currentURLSegment;
+    }
+
+    private function createFilteredUrlSegment(string $content): string
+    {
+        if (!$content) {
+            throw new Exception("The content is invalid or null");
+        }
+
+        $filter = URLSegmentFilter::create();
+        return $filter->filter($content);
+    }
+
+    public function generateUrlSegment(string $title): string
+    {
+        $filteredTitle = $this->createFilteredUrlSegment($title);
+        $className = strtolower($this->owner->ClassName);
+
+        return $filteredTitle && $filteredTitle !== '-' ? $filteredTitle : "{$className}-{$this->owner->ID}";
+    }
+}
